ac92387900403c8ed8ac19e32d68c62ec162ba76
Théo <theo.rgr2404@gmail.com>
2025-08-17 13:20:28 -0700

    Initial commit : ajout du projet Symfony

---
+++ src/Controller/CandidatureController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Candidature;
+use App\Form\CandidatureType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CandidatureController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function home(EntityManagerInterface $em): Response
+    {
+        $candidatures = $em->getRepository(Candidature::class)->findAll();
+
+        return $this->render('candidature/index.html.twig', [
+            'candidatures' => $candidatures,
+        ]);
+    }
+
+    #[Route('/candidature/new', name: 'candidature_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $candidature = new Candidature();
+        
+        $form = $this->createForm(CandidatureType::class, $candidature);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($candidature);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('candidature/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Entity/Candidature.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CandidatureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CandidatureRepository::class)]
+class Candidature
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lien = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $statut = null;
+
+    public function __construct()
+    {
+        $this->statut = 'Candidature';
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getLien(): ?string
+    {
+        return $this->lien;
+    }
+
+    public function setLien(string $lien): static
+    {
+        $this->lien = $lien;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+}
